eccd408b1ee2f3ed3d2915c5ba020e2487d1fcb3
venomcat7 <bull-000@hotmail.com>
2025-01-16 17:36:49 -0500

    startgame navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import StartScreen from "./components/StartScreen";
+import GameScreen from "./components/GameScreen";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<StartScreen />} />
+        <Route path="/game" element={<GameScreen />} />
       </Routes>
     </Router>
   );
--- frontend/src/components/StartScreen.js
+++ frontend/src/components/StartScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const StartScreen = ({ onStartGame }) => {
+  const navigate = useNavigate();
+
+  const handleStartGame = () => {
+    navigate("/game");
+  };
+
   return (
     <div>
       <h1>¡El reto del inspector!!!</h1>
-      <button onClick={onStartGame}>Comenzar Juego!</button>
+      <button onClick={handleStartGame}>Comenzar Juego!</button>
     </div>
   );
 };
